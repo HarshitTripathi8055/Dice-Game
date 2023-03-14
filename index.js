4d13d5945f55046f3a5bbe64d5ba17caa418f2f4
Harshit Tripathi <harshit.tripathi8055@gmail.com>
2023-03-14 16:59:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+var randomNumber1 = Math.floor(Math.random()*6) +1;  // 1-6
+
+var randomDiceImage = "dice" + randomNumber1 + ".png"; // dice1.png - dice2.png
+
+var randomImageSource = "images/" + randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src",randomImageSource);
+
+
+
+
+var randomNumber2 = Math.floor(Math.random()*6) +1;  // 1-6
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
+
+
+if(randomNumber1>randomNumber2)
+{
+    document.querySelector("h1").innerHTML = "🏆Player1 Wins";
+}
+
+
+if(randomNumber1<randomNumber2)
+{
+    document.querySelector("h1").innerHTML = "Player2 Wins🏆";
+}
+
+
+if(randomNumber1===randomNumber2)
+{
+    document.querySelector("h1").innerHTML = "Match Drawn";
+}
